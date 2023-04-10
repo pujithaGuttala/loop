96bb33463c26a522ad7fb621428f9aea0b88fff3
pujithaGuttala <108428114+pujithaGuttala@users.noreply.github.com>
2023-04-10 16:12:52 +0530

    Add files via upload

---
+++ 1.a.Patterns.py
@@ -0,0 +1,22 @@
+# n=int(input("enter the number"))
+# i=1                                      
+# while i<=n:
+#     j=1
+#     while j<=i:
+#         print(j,end=" ")
+#         j=j+1
+#     i=i+1
+#     print()    
+
+
+# i=0
+# while i<=300:
+#     i=i+10
+#     print(i)
+    
+       
+
+
+
+        
+
